2c997ed9845625af01e78a9e189d49f0814cc760
ramdanriawan <ramdanriawan3@gmail.com>
2023-04-29 23:12:45 +0700

    1

--- cognotive.sql
+++ cognotive.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb
--- Generation Time: Apr 29, 2023 at 06:46 AM
+-- Generation Time: Apr 29, 2023 at 04:12 PM
 -- Server version: 10.5.9-MariaDB-1:10.5.9+maria~focal
 -- PHP Version: 8.1.16
 
@@ -42,8 +42,13 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(0, 'Admin', 'admin@gmail.com', 'admin', '2023-04-28 12:27:16', '2023-04-28 12:27:16', NULL),
 (1, 'user test', 'coderindo2@gmail.com', 'user test', '2023-04-28 12:27:16', '2023-04-28 12:27:16', NULL),
-(2, 'user test', 'coderindo5@gmail.com', 'user test', '2023-04-28 12:27:16', '2023-04-28 21:00:47', NULL);
+(2, 'user test', 'usertest4@gmail.com', 'user test', '2023-04-28 12:27:16', '2023-04-29 22:02:12', NULL),
+(6, 'user test', 'usertest@gmail.com', 'user test', '2023-04-29 14:00:09', '2023-04-29 14:00:09', NULL),
+(8, 'user test', 'usertest2@gmail.com', 'user test', '2023-04-29 15:04:12', '2023-04-29 15:04:12', NULL),
+(10, 'user test', 'usertest222@gmail.com', 'user test', '2023-04-29 15:04:12', '2023-04-29 15:04:12', '2023-04-29 15:09:16'),
+(15, 'user test', 'usertest2@gmail.comxxxx', 'user test', '2023-04-29 21:59:41', '2023-04-29 21:59:41', '2023-04-29 22:03:40');
 
 -- --------------------------------------------------------
 
@@ -66,7 +71,11 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `customer_id`, `date`, `status`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(2, 1, '2023-04-28 00:00:00', 'Pending', '2023-04-28 12:32:29', '2023-04-29 11:47:30', NULL);
+(2, 2, '2023-04-28 00:00:00', 'Pending', '2023-04-28 12:32:29', '2023-04-29 22:16:15', NULL),
+(11, 1, '0000-00-00 00:00:00', 'Pending', '2023-04-29 15:26:51', '2023-04-29 15:26:51', '2023-04-29 22:16:55'),
+(12, 8, '0000-00-00 00:00:00', 'Pending', '2023-04-29 15:26:51', '2023-04-29 15:26:51', '2023-04-29 22:13:50'),
+(13, 2, '0000-00-00 00:00:00', 'Pending', '2023-04-29 22:15:07', '2023-04-29 22:15:07', NULL),
+(14, 2, '0000-00-00 00:00:00', 'Pending', '2023-04-29 22:15:20', '2023-04-29 22:15:20', NULL);
 
 -- --------------------------------------------------------
 
@@ -91,8 +100,18 @@ CREATE TABLE `order_details` (
 --
 
 INSERT INTO `order_details` (`id`, `order_id`, `product_id`, `qty`, `price`, `total`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(15, 2, 2, 2, 11, 22, '2023-04-28 23:09:08', '2023-04-28 23:09:08', NULL),
-(16, 2, 1, 2, 10, 20, '2023-04-28 23:09:10', '2023-04-28 23:09:10', NULL);
+(15, 2, 2, 2, 11, 22, '2023-04-28 23:09:08', '2023-04-28 23:09:08', '2023-04-29 14:02:43'),
+(16, 2, 1, 2, 10, 20, '2023-04-28 23:09:10', '2023-04-28 23:09:10', '2023-04-29 14:02:43'),
+(17, 2, 2, 2, 11, 22, '2023-04-29 14:02:44', '2023-04-29 14:02:44', '2023-04-29 15:27:52'),
+(18, 2, 2, 2, 11, 22, '2023-04-29 14:02:46', '2023-04-29 14:02:46', '2023-04-29 15:27:52'),
+(19, 11, 2, 2, 11, 22, '2023-04-29 15:26:52', '2023-04-29 15:26:52', NULL),
+(20, 2, 2, 2, 11, 22, '2023-04-29 15:27:53', '2023-04-29 15:27:53', '2023-04-29 22:16:16'),
+(21, 2, 2, 2, 11, 22, '2023-04-29 15:27:55', '2023-04-29 15:27:55', '2023-04-29 22:16:16'),
+(22, 12, 2, 2, 11, 22, '2023-04-29 15:27:55', '2023-04-29 15:27:55', NULL),
+(23, 13, 2, 2, 11, 22, '2023-04-29 22:15:08', '2023-04-29 22:15:08', NULL),
+(24, 14, 2, 2, 11, 22, '2023-04-29 22:15:21', '2023-04-29 22:15:21', NULL),
+(25, 2, 2, 2, 11, 22, '2023-04-29 22:16:17', '2023-04-29 22:16:17', NULL),
+(26, 2, 2, 2, 11, 22, '2023-04-29 22:16:19', '2023-04-29 22:16:19', NULL);
 
 -- --------------------------------------------------------
 
@@ -116,8 +135,10 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'product 1', 10, 'Is a product 1', 'https://bikinaplikasi.dev/image1.png', '2023-04-28 13:19:26', '2023-04-28 13:19:26', '2023-04-28 13:19:26'),
-(2, 'product 2', 11, 'Is a product 2', 'https://bikinaplikasi.dev/image2.png', '2023-04-28 13:19:26', '2023-04-28 13:19:26', '2023-04-28 13:19:26');
+(1, 'product 1', 10, 'Is a product 3', 'https://bikinaplikasi.dev/image1.png', '2023-04-28 13:19:26', '2023-04-28 13:19:26', '2023-04-28 13:19:26'),
+(2, 'product 2', 11, 'Is a product 2', 'https://bikinaplikasi.dev/image2.png', '2023-04-28 13:19:26', '2023-04-28 13:19:26', '2023-04-28 13:19:26'),
+(6, 'product 4', 10, 'Is a product 1', 'https://bikinaplikasi.dev/image1.png', '2023-04-29 07:01:16', '2023-04-29 07:01:16', '2023-04-29 07:01:16'),
+(9, 'product 444', 10, 'Is a product 1', 'https://bikinaplikasi.dev/image1.png', '2023-04-29 15:05:40', '2023-04-29 15:05:40', '2023-04-29 15:05:40');
 
 --
 -- Indexes for dumped tables
@@ -160,25 +181,25 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Constraints for dumped tables
